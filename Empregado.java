bc7881bd1d2a7518cf6a2c7e4a1bea3cfcf94bfa
Fernando Meca <fernando_mecca@hotmail.com>
2021-08-04 13:44:54 -0300

    Commit inicial. Classe Empregado criada

---
+++ Empregado.java
@@ -0,0 +1,20 @@
+public class Empregado{
+    private String nome;
+    private int idade;
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setNome(String nome){
+        this.nome=nome;
+    }
+
+    public String getIdade(){
+        return this.idade;
+    }
+
+    public void setIdade(String idade){
+        this.idade=idade;
+    }
+}
